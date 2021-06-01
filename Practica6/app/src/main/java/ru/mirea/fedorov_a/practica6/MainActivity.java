343c1bf95d7a50cd5c4ff66d11efb69f42addf20
enot008 <lexichfiodorov@yandex.ru>
2021-06-01 20:09:07 +0300

    add pr6

---
+++ Practica6/app/src/main/java/ru/mirea/fedorov_a/practica6/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.mirea.fedorov_a.practica6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
